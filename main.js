9db1b71ee589a756af392cb4af5c78a23195e03a
kevin-rojas <kevin.rojascuritima@unas.edu.pe>
2022-12-18 09:43:42 -0500

    v3.0

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import {Player} from "./player.js"
-
-
+import { ImputHandler } from "./imputs.js"
+import { Background } from "./backgrounds.js"
+import { FlyingEnemy,GroundEnemy,ClimbingEnemy } from "./enemies.js"
 window.addEventListener('load',function(){
    const canvas= this.document.getElementById('canvas1')
    const ctx = canvas.getContext('2d')
@@ -8,25 +9,64 @@ window.addEventListener('load',function(){
    canvas.height=500
 
    class Game{
-    constructor(width,height){
+      constructor(width,height){
        this.width=width
        this.height=height
+       this.groundMargin=80   
+       this.speed=0
+       this.backgrund= new Background(this)
        this.player= new Player(this)
-    }
-    update(){
+       this.input= new ImputHandler()
+       this.enemies=[]
+       this.enemyTimer=0
+       this.enemyInterval=1000
+      }
+      update(deltaTime){
+        this.backgrund.update()
+        this.player.update(this.input.keys,deltaTime)
+        //HandleEnemy
+        if(this.enemyTimer > this.enemyInterval){
+            this.addEnemy()
+            this.enemyTimer = 0
+         } else {
+           this.enemyTimer += deltaTime
+         }
+         this.enemies.forEach(enemy =>{
+            enemy.update(deltaTime)
+            if(enemy.markedForDeletion) this.enemies.splice(this.enemies.indexOf(enemy),1)
+        })
 
-    }         
-    draw(context){ 
-     this.player.draw(context)
-    }
+      }         
+     draw(context){ 
+       this.backgrund.draw(context)
+       this.player.draw(context)
+       this.enemies.forEach(enemy =>{
+          enemy.draw(context)
+       })
+     }       
+     addEnemy(){
+       this.enemies.push(new FlyingEnemy(this))
+       console.log(this.enemies)
+     }
+
+
    }
-   const game=new Game( canvas.width, canvas.height )
+   
+   
+   const game = new Game( canvas.width, canvas.height )
    
    console.log(game)
-   function animate() {
+   let lasTime=0
+
+   function animate(timeStamp) {
+      const deltaTime=timeStamp - lasTime
+      lasTime=timeStamp
+      ctx.clearRect(0,0,canvas.width,canvas.height)
+      game.update(deltaTime) 
       game.draw(ctx)
       requestAnimationFrame(animate)
    }
-   animate()
+   animate(0)
+   
 
 })
--- player.js
+++ player.js
@@ -1,18 +1,60 @@
+import { Sitting , Running,Jumping,Falling } from "./playerStates.js"
+
 export class Player{
    constructor(game){
     this.game=game
     this.width=100
     this.height=91.3
     this.x=0
-    this.y=this.game.width-this.height 
-    this.image=document.getElementById('player')
-   }
-   update(){
-
+    this.y= this.game.width - this.height - this.game.groundMargin
+    this.vy=0
+    this.weight=1
+    this.img=document.getElementById('player')  
+    this.frameX=0
+    this.frameY=0
+    this.maxFrame=5
+    this.fps=20
+    this.frameInterval=1000/this.fps
+    this.frameTimer=0
+    this.speed=0
+    this.maxSpeed=10
+    this.state=[new Sitting(this), new Running(this),new Jumping(this),new Falling(this)]
+    this.currenState=this.state[0]
+    this.currenState.enter()
+   };
+   update(input,deltaTime){
+      this.currenState.handleInput(input)
+      //movimiento horizontal
+      this.x += this.speed 
+      if(input.includes('ArrowRight')) this.speed=this.maxSpeed
+      else if(input.includes('ArrowLeft'))this.speed =- this.maxSpeed
+      else this.speed=0
+      if(this.x<0) this.x=0
+      if(this.x>this.game.width - this.width)this.x = this.game.width - this.width
+      //movimiento vertical
+      //if(input.includes('ArrowUp')&& this.onGround()) this.vy -= 20
+      this.y += this.vy
+      if(!this.onGround()) this.vy += this.weight
+      else this.vy=0
+      //sprite animacion 
+      if(this.frameTimer > this.frameInterval){
+         this.frameTimer=0
+         if(this.frameX< this.maxFrame)this.frameX ++
+         else this.frameX=0
+      }else{
+         this.frameTimer += deltaTime
+      }
    }
    draw(context){
-      context.fillStyle ="red"
-      context.fillRect(this.x, this.y, this.width, this.height)
-      context.drawImage(this.image,this.x,this.y)
+     context.drawImage(this.img,this.frameX * this.width,this.frameY * this.height,this.width,this.height,this.x,this.y,this.width,this.height);
+   }
+   onGround(){
+      return   this.y >= this.game.height - this.height - this.game.groundMargin
+   }
+   
+   setState(state,speed){
+      this.currenState  = this.state[state]
+      this.game.speed=speed
+      this.currenState.enter()
    }
 }
